7799033d6f405b7491a2cc08f227e6cf9d6dd782
Vineeth <vineeth.v.gooli@gmail.com>
2023-11-20 22:02:22 +0530

    rendering stopped on small devices, key frames changed

--- src/Models/DirtCar.jsx
+++ src/Models/DirtCar.jsx
@@ -9,9 +9,9 @@ import {
 
 import carState from "../CarState.json";
 import Overlay from "../Components/Overlay";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 import { CanvasLoader } from "../Components/CanvasLoader";
-import { Volume2, VolumeX } from "lucide-react";
+import { Play, Pause } from "lucide-react";
 import { useState } from "react";
 
 const DirtCar = () => {
@@ -19,57 +19,117 @@ const DirtCar = () => {
   const [play, setPlay] = useState(false);
 
   const sheet = getProject("Dirt Car", { state: carState }).sheet("Sheet");
-  return (
-    <div>
-      {play ? (
-        <button
-          className="fixed top-3 right-4 z-[99999] border-black border py-1 px-2"
-          onClick={() => {
-            if (play) {
-              setPlay(false);
-              vAudio.pause();
-            }
-          }}
-        >
-          <VolumeX className="h-5 w-5" />
-        </button>
-      ) : (
-        <button
-          className="fixed top-3 right-4 z-[99999]  border-black border py-1 px-2"
-          onClick={() => {
-            if (!play) {
-              setPlay(true);
-              vAudio.play();
-            }
-          }}
-        >
-          <Volume2 className="h-5 w-5" />
-        </button>
-      )}
 
-      <audio id="divAudio" loop>
-        <source src="/fool.mp3" type="audio/mp3"></source>
-      </audio>
-      <div className="w-screen h-screen hidden lg:block">
-        <Canvas gl={{ preserveDrawingBuffer: true }}>
-          <Suspense fallback={<CanvasLoader />}>
-            <ScrollControls pages={5}>
-              <Overlay />
-              <SheetProvider sheet={sheet}>
-                <Scene />
-              </SheetProvider>
-            </ScrollControls>
-          </Suspense>
-        </Canvas>
-      </div>
+  useEffect(() => {
+    const vAudio = document.getElementById("divAudio");
+    if (vAudio) {
+      if (play) {
+        vAudio.play().catch((error) => {
+          console.error("Error playing audio:", error);
+        });
+      } else {
+        vAudio.pause();
+      }
+    }
+  }, [play]);
 
-      <div className="flex lg:hidden h-screen w-screen justify-center items-center">
-        <p className="text-4xl font-extrabold text-center">
-          Sorry Mate! Only Available on large devices ;)
-        </p>
+  const isLargeDevice = window.innerWidth > 1024;
+
+  if (!isLargeDevice) {
+    return (
+      <div>
+        {play ? (
+          <button
+            className="fixed top-3 right-4 z-[99999] border-black border py-1 px-2"
+            onClick={() => {
+              if (play) {
+                setPlay(false);
+                vAudio.pause();
+              }
+            }}
+          >
+            <Pause className="h-5 w-5" />
+          </button>
+        ) : (
+          <button
+            className="fixed top-3 right-4 z-[99999]  border-black border py-1 px-2"
+            onClick={() => {
+              if (!play) {
+                setPlay(true);
+                vAudio.play();
+              }
+            }}
+          >
+            <Play className="h-5 w-5" />
+          </button>
+        )}
+
+        <audio id="divAudio" loop>
+          <source src="/fool.mp3" type="audio/mp3"></source>
+        </audio>
+
+        <div className="flex flex-col lg:hidden h-screen w-screen justify-center items-center">
+          <p className="text-4xl font-extrabold text-center">
+            Sorry Mate! Only Available on large devices :(
+          </p>
+
+          <a
+            href="https://github.com/idebenone"
+            target="_blank"
+            className="font-bold text-xl mt-4 text-gray-600"
+          >
+            Vineeth G
+          </a>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div>
+        {play ? (
+          <button
+            className="fixed top-3 right-4 z-[99999] border-black border py-1 px-2"
+            onClick={() => {
+              if (play) {
+                setPlay(false);
+                vAudio.pause();
+              }
+            }}
+          >
+            <Pause className="h-5 w-5" />
+          </button>
+        ) : (
+          <button
+            className="fixed top-3 right-4 z-[99999]  border-black border py-1 px-2"
+            onClick={() => {
+              if (!play) {
+                setPlay(true);
+                vAudio.play();
+              }
+            }}
+          >
+            <Play className="h-5 w-5" />
+          </button>
+        )}
+
+        <audio id="divAudio" loop>
+          <source src="/fool.mp3" type="audio/mp3"></source>
+        </audio>
+        <div className="w-screen h-screen hidden lg:block">
+          <Canvas gl={{ preserveDrawingBuffer: true }}>
+            <Suspense fallback={<CanvasLoader />}>
+              <ScrollControls pages={5}>
+                <Overlay />
+                <SheetProvider sheet={sheet}>
+                  <Scene />
+                </SheetProvider>
+              </ScrollControls>
+            </Suspense>
+          </Canvas>
+        </div>
+      </div>
+    );
+  }
 };
 useGLTF.preload("/che.glb");
 
